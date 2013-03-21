2c5f8d12fafa6bd7e77326ff5f9f620bac1d177d
cancerberoSgx <sebastigurin@gmail.com>
2013-03-21 12:24:33 -0300

    momentgwt test1

---
+++ momentgwttest/momentgwttest1/src/org/sgx/momentgwttest1/client/MomentGwtTest1EP.java
@@ -0,0 +1,20 @@
+package org.sgx.momentgwttest1.client;
+
+import java.util.Date;
+
+import org.sgx.momentgwt.client.Moment;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class MomentGwtTest1EP implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		test1();
+	} 
+	private void test1() {
+		System.out.println(Moment.moment());
+		System.out.println(Moment.moment(new Date()).add(2, "days").format("MMMM Do YYYY, h:mm:ss a"));
+	}
+
+}
