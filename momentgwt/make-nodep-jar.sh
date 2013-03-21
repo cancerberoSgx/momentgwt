d00a5a7b7656ba6fb4dbae9b8b12053cd32e3995
cancerberoSgx <sebastigurin@gmail.com>
2013-03-21 15:52:16 -0300

    readme updated, make-nodep-jar.sh

---
+++ momentgwt/make-nodep-jar.sh
@@ -0,0 +1,16 @@
+#generates a .jar with all dependencies packaged inside. 
+#author: sgurin
+
+gwtjsutilJar=/home/sebastian/workspace 3.7rc2/gwtjsutil/target/gwtjsutil-1.0.jar
+momentgwtJar=/home/sebastian/git/momentgwt/momentgwt/target/momentgwt-1.0.jar
+outputJarName=momentgwt-1.0-alldeps.jar
+
+rm -rf jartmp
+mkdir jartmp
+cd jartmp
+unzip -u $gwtjsutilJar
+unzip -u $momentgwtJar
+zip -r $outputJarName *
+mv $outputJarName ../target
+cd ..
+rm -rf jartmp
