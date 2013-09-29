d08977de6c04ac8536ca434cd5024d62fd07fb61
cancerberoSgx <sebastigurin@gmail.com>
2013-09-29 18:17:32 +0000

    fixed pom

--- momentgwt/make-nodep-jar.sh
+++ momentgwt/make-nodep-jar.sh
@@ -1,15 +1,17 @@
 #generates a .jar with all dependencies packaged inside. 
 #author: sgurin
 
-gwtjsutilJar="/home/sebastian/workspace 3.7rc2/gwtjsutil/target/gwtjsutil-1.0.jar"
-momentgwtJar="/home/sebastian/git/momentgwt/momentgwt/target/momentgwt-1.0.jar"
-outputJarName=momentgwt-1.0-alldeps.jar
+gwtjsutilJar="/home/sg/workspace/gwtjsutil/target/gwtjsutil-1.0.jar" #must be absolute
+momentgwtJar="/home/sg/git/momentgwt/momentgwt/target/momentgwt-1.0.jar" #must be absolute
+outputJarName=/home/sg/git/momentgwt/momentgwt/target/momentgwt-1.0-alldeps.jar #must be absolute
 
 rm -rf jartmp
 mkdir jartmp
 cd jartmp
 unzip -u "$gwtjsutilJar"
 unzip -u "$momentgwtJar"
+
+#echo "" > $outputJarName
 zip -r $outputJarName *
 mv $outputJarName ../target
 cd ..
--- momentgwttest/momentgwttest1/src/org/sgx/momentgwttest1/client/MomentGwtTest1EP.java
+++ momentgwttest/momentgwttest1/src/org/sgx/momentgwttest1/client/MomentGwtTest1EP.java
@@ -2,10 +2,14 @@ package org.sgx.momentgwttest1.client;
 
 import java.util.Date;
 
+import org.sgx.jsutil.client.DOMUtil;
+import org.sgx.jsutil.client.JsUtil;
 import org.sgx.momentgwt.client.Moment;
 import org.sgx.momentgwttest1.client.tests.InternationalizationTest1;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.Window;
 
 public class MomentGwtTest1EP implements EntryPoint {
 
@@ -13,13 +17,26 @@ public class MomentGwtTest1EP implements EntryPoint {
 	public void onModuleLoad() {
 		test1();
 	} 
+	StringBuffer log= new StringBuffer(); 
 	private void test1() {
-		System.out.println(Moment.moment());
-		System.out.println(Moment.moment(new Date()).add(2, "days").format("MMMM Do YYYY, h:mm:ss a"));
-		System.out.println("from french: "+ Moment.moment("2012 juillet", "YYYY MMM", "fr"));
-		System.out.println("utc: "+Moment.moment().utc());
-		System.out.println("utc2: "+Moment.moment().utc(new int[]{2010, 1, 14, 15, 25, 50, 125}));		
+//		JsUtil.consoleLog("HELLO");
+		log(Moment.moment()); 
+		log(Moment.moment());
+		log(Moment.moment(new Date()).add(2, "days").format("MMMM Do YYYY, h:mm:ss a"));
+		log("from french: "+ Moment.moment("2012 juillet", "YYYY MMM", "fr"));
+		log("utc: "+Moment.utc());
+		log("utc2: "+Moment.utc(new int[]{2010, 1, 14, 15, 25, 50, 125}));
+		printLog();
 //		new InternationalizationTest1().test1(); 
+//		Window.alert("done2");
+	}
+	private void printLog() {
+		DOMUtil.appendStringAsNodes(Document.get().getBody(), "<p>"+log+"/p>"); 
+	}
+	private void log(Object s) {
+		log.append(s); 
+		log.append("</br>"); 
 	}
 
 }
+ 
